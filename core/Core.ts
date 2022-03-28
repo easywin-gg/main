ca0bfbc41c40187cdac8f376410836f12b61a69e
Nospher <jooownyy@gmail.com>
2022-03-28 20:26:51 -0300

    Create scripts folder if not exists

--- core/Core.ts
+++ core/Core.ts
@@ -1,6 +1,7 @@
 import { Module, Process } from 'memoryjs';
 import DrawManager from './draw/DrawManager';
 import Game from './game/Game';
+import fs from 'fs';
 
 class Core {
 
@@ -18,6 +19,10 @@ class Core {
 
         this.draw = new DrawManager();
         this.game = new Game(this);
+
+        if (!fs.existsSync(`${Core.MAIN_FOLDER_PATH}/scripts`)) {
+            fs.mkdirSync(`${Core.MAIN_FOLDER_PATH}/scripts`);
+        }
     }
 
     public static readIntegerFromBuffer(buffer: Buffer, offset: number) {
